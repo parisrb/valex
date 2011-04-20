5d89a9066acc6c58c92a5483e7ef35f1369e472c
Julien Kirch <code@archiloque.net>
2011-04-20 21:43:51 +0200

    use attributes

--- lib/valex/validations/numericality.rb
+++ lib/valex/validations/numericality.rb
@@ -7,23 +7,23 @@ module Valex::Validations
 
     def initialize
       # Specify a value the field must be exactly.
-      equal_to = options[:equal_to] if options[:equal_to]
+      @equal_to = options[:equal_to] if options[:equal_to]
       # Set that the value must be even.
-      even = options[:even] if options[:even]
+      @even = options[:even] if options[:even]
       # Specify a value the field must be greater than.
-      greater_than = options[:greater_than] if options[:greater_than]
+      @greater_than = options[:greater_than] if options[:greater_than]
       # Specify a value the field must be greater than or equal to.
-      greater_than_or_equal_to = options[:greater_than_or_equal_to] if options[:greater_than_or_equal_to]
+      @greater_than_or_equal_to = options[:greater_than_or_equal_to] if options[:greater_than_or_equal_to]
       # Specify a value the field must be less than.
-      less_than = options[:less_than] if options[:less_than]
+      @less_than = options[:less_than] if options[:less_than]
       # Specify a value the field must be less than or equal to.
-      less_than_or_equal_to = options[:less_than_or_equal_to] if options[:less_than_or_equal_to]
+      @less_than_or_equal_to = options[:less_than_or_equal_to] if options[:less_than_or_equal_to]
       # Set that the value must be odd.
-      odd = options[:odd] if options[:odd]
+      @odd = options[:odd] if options[:odd]
       # Set whether the value has to be an integer.
-      only_integer = options[:only_integer] if options[:only_integer]
+      @only_integer = options[:only_integer] if options[:only_integer]
       # Supply a custom error message.
-      message = options[:message] if options[:message]
+      @message = options[:message] if options[:message]
     end
 
     def to_json(*a)
