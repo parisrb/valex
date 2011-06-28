702626830f2101855894e4986a5a05ee286abe11
Julien Kirch <code@archiloque.net>
2011-06-28 19:53:40 +0200

    extract length validation in method and add process_numericality_validation

--- lib/valex/exporters/jquery.rb
+++ lib/valex/exporters/jquery.rb
@@ -16,6 +16,7 @@ module Valex::Exporters
     def process_to_ruby(models)
       result = {}
       models.each do |model|
+        # will contain the parameters to
         model_result = {}
         model.attributes.each_value do |attribute|
           attribute.validations.each do |validation|
@@ -25,16 +26,9 @@ module Valex::Exporters
               when Valex::Validations::Unique
                 # not available
               when Valex::Validations::Length
-                if validation.allow_blank
-                  model_result[:required] = true
-                end
-                if validation.minimum && validation.maximum
-                  model_result[:rangelength] = [validation.minimum, validation.maximum]
-                elsif validation.maximum
-                  model_result[:maxlength] = validation.maximum
-                elsif validation.minimum
-                  model_result[:minlength] = validation.minimum
-                end
+                process_length_validation(validation, model_result)
+              when Valex::Validations::Numericality
+                process_numericality_validation(validation, model_result)
               else
                 warn "Unknown validation #{validation}"
             end
@@ -44,5 +38,48 @@ module Valex::Exporters
       end
       result
     end
+  end
+
+  private
+
+  # length validation
+  def process_length_validation(validation, model_result)
+    if validation.allow_blank
+      model_result[:required] = false
+    end
+    if validation.minimum && validation.maximum
+      model_result[:rangelength] = [validation.minimum, validation.maximum]
+    elsif validation.maximum
+      model_result[:maxlength] = validation.maximum
+    elsif validation.minimum
+      model_result[:minlength] = validation.minimum
+    end
+  end
+  
+  # numericality validation
+  def process_numericality_validation(validation, model_result)
+    if validation.equal_to
+      model_result[:range] = [validation.equal_to, validation.equal_to]
+    end
+
+    if validation.greater_than_or_equal_to
+      model_result[:min] = validation.greater_than_or_equal_to
+    end
+    if validation.less_than_or_equal_to
+      model_result[:max] = validation.less_than_or_equal_to
+    end
+
+    if validation.greater_than
+      model_result[:min] = validation.greater_than
+    end
+    if validation.less_than
+      model_result[:max] = validation.less_than
+    end
+
+    # not perfect but best we can do without adding custom code
+    if validation.only_integer
+      model_result[:number] = validation.only_integer
+    end
+
   end
 end
